b52ec6a237b82c37d656f49cc6c6b27112253372
Steven Blom <atomicblom@binaryvibrance.net>
2015-01-13 02:03:32 +1030

    Replace getMappings with replaceMapping to help prevent people from shooting themselves in the foot.

---
+++ src/api/java/com/github/lunatrius/schematica/api/DuplicateMappingException.java
@@ -0,0 +1,8 @@
+package com.github.lunatrius.schematica.api;
+
+public class DuplicateMappingException extends Exception {
+
+    public DuplicateMappingException(String s) {
+        super(s);
+    }
+}
--- src/api/java/com/github/lunatrius/schematica/api/PreSchematicSaveEvent.java
+++ src/api/java/com/github/lunatrius/schematica/api/PreSchematicSaveEvent.java
@@ -1,5 +1,6 @@
 package com.github.lunatrius.schematica.api;
 
+import com.sun.javaws.exceptions.InvalidArgumentException;
 import cpw.mods.fml.common.eventhandler.Event;
 import net.minecraft.nbt.NBTTagCompound;
 
@@ -10,14 +11,23 @@ public class PreSchematicSaveEvent extends Event {
     private final NBTTagCompound extendedMetadata;
 
     public PreSchematicSaveEvent(Map<String, Short> mappings) {
-
-
         this.mappings = mappings;
         this.extendedMetadata = new NBTTagCompound();
     }
 
-    public Map<String, Short> getMappings() {
-        return mappings;
+    public boolean replaceMapping(String oldName, String newName) throws DuplicateMappingException {
+        if (this.mappings.containsKey(newName)) {
+            throw new DuplicateMappingException("You the block type %s already exists in the schematic. If this is intended, you should replace the blocks in the PostSchematicCaptureEvent before attempting a mapping change.");
+        }
+
+        Short id = this.mappings.get(oldName);
+        if (id != null) {
+            this.mappings.remove(oldName);
+            this.mappings.put(newName, id);
+            return true;
+        }
+
+        return false;
     }
 
     public NBTTagCompound getExtendedMetadata() {
